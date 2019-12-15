f405a23cab26b09605a760bee9415791feb80345
Abel Garcia <abelgarcia@banwire.com>
2019-12-14 23:44:06 -0600

    delete lines commands

---
+++ examples/delete.line.js
@@ -0,0 +1,24 @@
+/**
+ * delete lines using the next commands 
+ * commands:
+ *      delete  lines selected  : ⇧ ⌘ L
+ */
+
+
+let NoDelete = ':)';
+let deleteThisLine = ':(';
+
+NoDelete = '1';
+deleteThisLine    = 'a';
+NoDelete = '1';
+deleteThisLine    = 'a';
+
+
+/**
+ * result
+ */
+
+let NoDelete = ':)';
+
+NoDelete = '1';
+NoDelete = '1';
