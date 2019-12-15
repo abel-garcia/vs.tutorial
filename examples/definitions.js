9f8997bd1e9b3275b69968337ff1d85751e7cc69
Abel Garcia <abelgarcia@banwire.com>
2019-12-14 23:28:38 -0600

    go to definition functions

---
+++ examples/definitions.js
@@ -0,0 +1,13 @@
+/**
+ * go to definition of function greet
+ * example :
+ *      fn ⌥ F12
+ *      fn F12
+ */
+
+
+ import {greet} from "./extra/functions"
+
+ const greeting = greet('Thanos');
+
+ console.log(greeting)
---
+++ examples/extra/functions.js
@@ -0,0 +1,86 @@
+
+
+
+export function greet1( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet2( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet3( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet4( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet5( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet6( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet7( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet8( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet9( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet10( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet11( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet12( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet13( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet14( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet15( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet16( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet( nombre ) {
+    return `Hola ${ nombre }!!!`;
+}
+
+export function greet17( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet18( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet19( nombre ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function greet20( nombre ) {
+    return `Yo no hago nada!!!`;
+}
